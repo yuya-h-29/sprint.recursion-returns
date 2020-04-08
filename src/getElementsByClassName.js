11b894861ed6c77c12afd0f8b68e5b94ca8d64c1
Yuya Harada <tzou11291996@gmail.com>
2020-04-08 19:12:21 +0900

    first commit
    Co-authored-by: jbentleyjp <jbentleyjp@users.noreply.github.com>
    Co-authored-by: yuya-h-29 <yuya-h-29@users.noreply.github.com>

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,5 +1,19 @@
-const getElementsByClassName = () => {
-  // YOUR CODE HERE
+const getElementsByClassName = (className) => {
+  const results = [];
+
+  const searchClass = (element) => {
+    if (element.children === undefined) {
+      return;
+    }
+    if (element.classList && element.classList.contains(className)) {
+      results.push(element);
+    }
+    for (const currentElement of element.children) {
+      searchClass(currentElement);
+    }
+  };
+  searchClass(document);
+  return results;
 };
 
 module.exports = { getElementsByClassName };
